a520152aac7e4017321d21e83440a30698c55ff6
JD-7 <89059759+JD-7@users.noreply.github.com>
2024-01-09 23:53:54 +0530

    morse_code.py

--- morse_code.py
+++ morse_code.py
@@ -1,65 +1,77 @@
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
- 
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                  'C': '-.-.', 'D': '-..', 'E': '.',
+                  'F': '..-.', 'G': '--.', 'H': '....',
+                  'I': '..', 'J': '.---', 'K': '-.-',
+                  'L': '.-..', 'M': '--', 'N': '-.',
+                  'O': '---', 'P': '.--.', 'Q': '--.-',
+                  'R': '.-.', 'S': '...', 'T': '-',
+                  'U': '..-', 'V': '...-', 'W': '.--',
+                  'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                  '1': '.----', '2': '..---', '3': '...--',
+                  '4': '....-', '5': '.....', '6': '-....',
+                  '7': '--...', '8': '---..', '9': '----.',
+                  '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                  '?': '..--..', '/': '-..-.', '-': '-....-',
+                  '(': '-.--.', ')': '-.--.-'}
+
 
 def encrypt(message):
     cipher = ''
     for letter in message:
-        if letter != ' ':
- 
-            cipher += MORSE_CODE_DICT[letter] + ' '
-        else:
+        if letter.upper() in MORSE_CODE_DICT:
+            cipher += MORSE_CODE_DICT[letter.upper()] + ' '
+        elif letter == ' ':
             cipher += ' '
- 
+        else:
+            raise ValueError("Invalid character for encryption: '{}'".format(letter))
+
     return cipher
 
-def decrypt(message):
 
+def decrypt(message):
     message += ' '
- 
     decipher = ''
     citext = ''
     for letter in message:
- 
-        if (letter != ' '):
- 
+
+        if letter != ' ':
             i = 0
             citext += letter
- 
+
         else:
             i += 1
-            if i == 2 :
+            if i == 2:
                 decipher += ' '
             else:
-                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
-                .values()).index(citext)]
-                citext = ''
- 
+                try:
+                    decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                                                                   .values()).index(citext)]
+                    citext = ''
+                except ValueError:
+                    raise ValueError("Invalid Morse code: '{}'".format(citext))
+
     return decipher
- 
+
 
 def main():
-    message = "Day 2 Devpost"
-    result = encrypt(message.upper())
-    print (result)
- 
-    message = "--. . . -.- ... -....- ..-. --- .-. -....- --. . . -.- ... "
-    result = decrypt(message)
-    print (result)
- 
+    user_input = input("Enter text: ")
+    choice = input("Encrypt or Decrypt? (E/D): ")
+
+    if choice.upper() == 'E':
+        try:
+            result = encrypt(user_input)
+            print("Encrypted message:", result)
+        except ValueError as e:
+            print("Error:", e)
+    elif choice.upper() == 'D':
+        try:
+            result = decrypt(user_input)
+            print("Decrypted message:", result)
+        except ValueError as e:
+            print("Error:", e)
+    else:
+        print("Invalid choice. Please enter 'E' for encryption or 'D' for decryption.")
+
 
 if __name__ == '__main__':
     main()
